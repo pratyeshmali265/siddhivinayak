42ce578712cd9927a6feea249f5fcc847ad7d9d5
pratyeshmali265 <pratyeshmali265@gmail.com>
2025-10-28 19:56:44 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,5 @@
+import React, { useState } from "react";
+// Simplified entrypoint based on the SiddhivinayakSite component
+export default function App() {
+  return <div>Hello from Siddhivinayak Engineering website!</div>;
+}
